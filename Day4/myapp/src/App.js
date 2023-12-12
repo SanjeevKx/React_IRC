6b27ba0fabd96034a32aebacda2ddf8b2602df84
SanjeevKx <sanjeevsanju76429@gmail.com>
2023-12-12 16:27:22 +0530

    done

---
+++ Day4/myapp/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import StateX from './components/StateX'
+import Temp from './components/Temp'
+function App() {
+  return (
+    <div className="App">
+      <StateX/>
+      <Temp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day4/myapp/src/components/Temp.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+const Temp =()=> {
+    return(
+        <>
+         <form>
+            <input type="text"/>
+            <input type="password" />
+            <input type="submit"  value="login"/>
+         </form>
+        
+        </>
+    )
+}
+export default Temp
